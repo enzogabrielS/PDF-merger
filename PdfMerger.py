da9f16d4d68f7f3456f4885ec327d6559313c6c7
Enzo <cotiaco2.0@gmail.com>
2025-05-18 20:21:38 -0300

    Commit de envio

---
+++ PdfMerger.py
@@ -0,0 +1,12 @@
+import PyPDF2
+import os
+
+merger = PyPDF2.PdfMerger()
+arquive_list = os.listdir("arquivos")
+arquive_list.sort()
+for arquivo in arquive_list:
+    if arquivo.endswith(".pdf"):
+        merger.append(f"arquivos/{arquivo}")
+
+merger.write("PDF_final.pdf")
+print("O seu arquivo foi gerado com sucesso!")
