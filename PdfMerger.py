89f79716273f6fb884db19d0d7be3e0fa21877d7
Enzo Gabriel <160799288+enzogabrielS@users.noreply.github.com>
2025-05-22 10:48:13 -0300

    Update PdfMerger.py

--- PdfMerger.py
+++ PdfMerger.py
@@ -4,6 +4,7 @@ import os
 merger = PyPDF2.PdfMerger()
 arquive_list = os.listdir("arquivos")
 arquive_list.sort()
+
 for arquivo in arquive_list:
     if arquivo.endswith(".pdf"):
         merger.append(f"arquivos/{arquivo}")
